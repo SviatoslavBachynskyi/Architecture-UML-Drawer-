1af6601e575f7d016347841fde807860ecef9162
Sviatoslav Bachynskyi <ba4insckij.s@gmail.com>
2021-01-12 21:51:22 +0200

    save image in local storage

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,3 +1,4 @@
+import { ViewChild } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -11,6 +12,7 @@ import { TasksService } from 'src/app/services/tasks.service';
 export class AddTaskComponent implements OnInit {
   form: FormGroup;
   error: string;
+  @ViewChild('imageFile') imageFile;
 
   constructor(
     private fb: FormBuilder,
@@ -27,24 +29,46 @@ export class AddTaskComponent implements OnInit {
     });
   }
 
+  image: string;
+
+  trimLeft(initialString: string, stringToTrim: string): string {
+    return initialString.startsWith(stringToTrim) ? initialString.substring(stringToTrim.length) : initialString;
+  }
+
   onSubmit(): void {
     if (this.form.valid) {
-      const formInput = {
+      let fileInput = this.imageFile.nativeElement;
+      if (fileInput.files && fileInput.files[0]) {
+        let fileReader = new FileReader();
+        fileReader.onload = (e) => {
+          debugger;
+          let image = String(fileReader.result);
+          this.image = this.trimLeft(image, "data:image/png;base64,");
+
+          this.saveTask();
+        }
+
+        fileReader.readAsDataURL(fileInput.files[0]);
+      }
+
+    }
+  }
+  saveTask(): void {
+    const formInput = {
       title: this.form.get('title').value,
       type: this.form.get('type').value,
       level: this.form.get('level').value,
       description: this.form.get('description').value,
-      image: null
-      };
+      image: this.image,
+    };
 
-      const result = (this.taskService.addTask(formInput));
+    const result = (this.taskService.addTask(formInput));
 
-      if (!!result) {
-        this.error = null;
-        this.router.navigate(['create-etalon', result]).then();
-      } else {
-        this.error = 'Завдання з такою назвою вже існує';
-      }
+    if (!!result) {
+      this.error = null;
+      this.router.navigate(['create-etalon', result]).then();
+    } else {
+      this.error = 'Завдання з такою назвою вже існує';
     }
   }
 }
